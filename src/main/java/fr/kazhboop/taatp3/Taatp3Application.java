44bf3a8367eb6c59ae184358552f69505867c97a
Kazh-Boop <baptiste.lelievre56@gmail.com>
2023-11-26 20:59:01 +0100

    fix to launch app

--- src/main/java/fr/kazhboop/taatp3/Taatp3Application.java
+++ src/main/java/fr/kazhboop/taatp3/Taatp3Application.java
@@ -2,10 +2,12 @@ package fr.kazhboop.taatp3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("fr.kazhboop.taatp3.domain")
 public class Taatp3Application {
 
 	public static void main(String[] args) {
