16f991c3ed7c1cfe4c015a19815620ed71c780cc
Kazh-Boop <baptiste.lelievre56@gmail.com>
2023-11-26 20:59:16 +0100

    add AOP

--- src/main/java/fr/kazhboop/taatp3/aspect/LoggingAspect.java
+++ src/main/java/fr/kazhboop/taatp3/aspect/LoggingAspect.java
@@ -1,4 +1,40 @@
 package fr.kazhboop.taatp3.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * PointCut to log call on the API
+     */
+    @Pointcut("within(fr.kazhboop.taatp3.controller.*)")
+    public void inRestLayer() {
+    }
+
+    /**
+     * Inform the start of a method in the rest layer
+     * @param joinPoint
+     */
+    @Before("inRestLayer()")
+    public void logMethodStart(JoinPoint joinPoint){
+        log.debug("Executing the method " + joinPoint.getSignature());
+    }
+
+    /**
+     * return the info on the method (do not display info of the DTO)
+     * @param joinPoint
+     * @param result
+     */
+    @AfterReturning(pointcut = "inRestLayer()", returning = "result")
+    public void logAfter(JoinPoint joinPoint, Object result) {
+        log.debug("Method " + joinPoint.getSignature().getName() + " terminate" + " result : " + result);
+    }
 }
